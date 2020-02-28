b7102095d8e7ddb86ab7c797eb5f99aa71964ccb
gwang111 <garyawang1@gmail.com>
2020-02-28 13:03:48 -0500

    c

---
+++ hw4/3.sql
@@ -0,0 +1,21 @@
+SELECT gettheyears.yearid
+FROM (
+	SELECT DISTINCT topmanagers.yearid, teams.teamid
+	FROM (	
+		SELECT DISTINCT managers.playerid, managers.teamid, managers.yearid
+		FROM (
+			SELECT MAX(managers.w) AS wins, managers.yearid
+			FROM managers
+			WHERE managers.yearid >= 1975
+			GROUP BY managers.yearid
+		) AS dubs
+		JOIN managers
+			ON dubs.wins = managers.w
+		WHERE dubs.yearid = managers.yearid
+	) AS topmanagers
+	JOIN teams
+		ON topmanagers.teamid = teams.teamid
+	WHERE topmanagers.yearid = teams.yearid
+	AND teams.divwin = 'Y'
+	ORDER BY topmanagers.yearid DESC
+) gettheyears
