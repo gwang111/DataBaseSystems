bad36f7993f913b4586035abb89fb2f8b5b22559
gwang111 <garyawang1@gmail.com>
2020-02-28 18:46:32 -0500

    fixes

--- hw4/7.sql
+++ hw4/7.sql
@@ -1,7 +1,21 @@
-select yearid as year, sum(salary)/count(distinct teamid) as salary
-
-from salaries
-
-group by yearid
-
-order by yearid
+SELECT abalot.playerid, SUM(CAST(abalot.h AS DECIMAL)/CAST(abalot.totab AS DECIMAL)) AS career_avg
+FROM (
+	SELECT DISTINCT batting.playerid, batting.yearid, batting.teamid, batting.h, batting.ab, career.totab
+	FROM (
+		SELECT SUM(filteredb.ab) as totab, filteredb.playerid
+		FROM (
+			SELECT batting.playerid, batting.yearid, batting.teamid, batting.ab 
+			FROM master
+			JOIN batting
+				ON master.playerid = batting.playerid
+			WHERE master.birthyear >= 1958
+			AND master.birthyear <= 1960
+		) AS filteredb
+		GROUP BY filteredb.playerid
+	) AS career
+	JOIN batting
+		ON career.playerid = batting.playerid
+	WHERE career.totab > 300
+) AS abalot
+GROUP BY abalot.playerid
+ORDER BY SUM(CAST(abalot.h AS DECIMAL)/CAST(abalot.totab AS DECIMAL)) DESC
--- hw4/9.sql
+++ hw4/9.sql
@@ -1,14 +1,8 @@
-SELECT totteamsals.yearid AS year, AVG(totteamsals.teamtot) AS salary
-FROM (	
-	SELECT SUM(CAST(teamsals.salary AS DECIMAL)) as teamtot, teamsals.teamid, teamsals.yearid
-	FROM (
-		SELECT DISTINCT salaries.teamid, salaries.playerid, salaries.yearid, salaries.salary
-		FROM teams
-		JOIN salaries
-			ON teams.teamid = salaries.teamid
-		WHERE teams.yearid = salaries.yearid
-	) AS teamsals
-	GROUP BY teamsals.teamid, teamsals.yearid
-) AS totteamsals
-GROUP BY totteamsals.yearid
-ORDER BY totteamsals.yearid
+select yearid as year, sum(salary)/count(distinct teamid) as salary
+
+from salaries
+
+group by yearid
+
+order by yearid
+
