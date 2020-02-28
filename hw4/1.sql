35316ede63363593e535177430d7cfd97305f631
gwang111 <garyawang1@gmail.com>
2020-02-28 15:06:33 -0500

    f

---
+++ hw4/1.sql
@@ -0,0 +1,8 @@
+SELECT COUNT(DISTINCT totsv.playerid)
+FROM (
+	SELECT SUM(pitching.sv) as svs, pitching.playerid, pitching.yearid
+	FROM pitching
+	WHERE pitching.yearid >= 1975
+	GROUP BY pitching.yearid, pitching.playerid
+) AS totsv
+WHERE totsv.svs > 40
