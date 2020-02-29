3dd15db6f449f11252e76936d43e3a259e7a553e
gwang111 <garyawang1@gmail.com>
2020-02-28 22:03:12 -0500

    update

--- hw4/6.sql
+++ hw4/6.sql
@@ -3,8 +3,8 @@ FROM
 (
 	SELECT MAX(master.namefirst) AS first, MAX(master.namelast) AS last, COUNT(allstarfull.playerid) AS appearances, SUM(halloffame.votes) AS votes
 	FROM master
-	LEFT JOIN halloffame ON halloffame.playerid = master.playerid
-	LEFT JOIN allstarfull ON allstarfull.playerid = master.playerid
+	JOIN halloffame ON halloffame.playerid = master.playerid
+	JOIN allstarfull ON allstarfull.playerid = master.playerid
 	WHERE halloffame.yearid = 2000
 	GROUP BY allstarfull.playerid
 	ORDER BY COUNT(allstarfull.playerid) DESC, SUM(halloffame.votes) DESC
